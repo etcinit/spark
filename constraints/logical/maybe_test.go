9496f4dc0847adf23d0aeba1cbf3cdc0e98af25f
Eduardo Trujillo <ed@chromabits.com>
2015-11-01 13:29:08 -0500

    Add maybe_test.go

---
+++ constraints/logical/maybe_test.go
@@ -0,0 +1,18 @@
+package logical
+
+import (
+	"testing"
+
+	"github.com/etcinit/spec/constraints/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMaybeConstraintIsFulfilled(t *testing.T) {
+	current := MaybeConstraint{
+		&types.StringConstraint{},
+	}
+
+	assert.True(t, current.IsFulfilled("hello"))
+	assert.False(t, current.IsFulfilled(45))
+	assert.True(t, current.IsFulfilled(nil))
+}
